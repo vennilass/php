78deefcdf02a4fc285b56ff9b75a6e6cc0a35fa7
vennilass <39242715+vennilass@users.noreply.github.com>
2018-05-13 22:24:10 +0530

    Create doc1.php

---
+++ login/doc1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+a {text-decoration: none;
+color:black;
+}
+table{
+margin-left: auto;
+    margin-right: auto;
+}
+body  {
+    background-image: url("b.jpg");
+    background-color: #cccccc;
+	color:white;
+	font-size:50px;
+}
+</style>
+</head>
+<body>
+
+<h2><center>SKCET SMART HOSTEL</h2><br><br>
+<table style="width:50%">
+
+  <tr>
+    <td><center><img src="icon1.svg" alt="Flowers in Chania" width="200" height="200"></td>
+	<td><center><img src="icon2.svg" alt="Flowers in Chania" width="200" height="200"></td>
+    
+   
+  </tr>
+  <tr>
+		<td><center> <a href="new11.php">STUDENT</a> </td>
+    
+    <td><center> <a href="login.html">FACULTY</a> </td>
+  </tr>
+</table>
+
+</body>
+</html>
